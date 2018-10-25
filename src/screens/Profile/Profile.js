4e6a10b753f9f1d119090e4c5e85eb178193ff0d
Omer Khan <omerkhan842@gmail.com>
2018-10-25 13:45:02 +0500

    creating profile first time

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -5,6 +5,7 @@ import './Profile.css';
 import Loader from 'react-loader-spinner';
 import Biodata from '../../components/Biodata';
 import Picture from '../../components/Picture';
+import Details from '../../components/Details';
 
 const db = firebase.database();
 const auth = firebase.auth();
@@ -14,8 +15,15 @@ class Profile extends React.Component {
         super(props);
         this.state = {
             userData: null,
-            loader: true
+            loader: true,
+            Bio: true,
+            Pic: false,
+            Detail: false,
+            step: 1
         }
+        this.bioNext = this.bioNext.bind(this);
+        this.picNext = this.picNext.bind(this);
+        this.detailNext = this.detailNext.bind(this);
     }
 
     componentDidMount() {
@@ -45,36 +53,57 @@ class Profile extends React.Component {
 
     }
 
+    bioNext({ nickName, number }) {
+        let { step } = this.state;
+        this.setState({ nickName, number, Bio: false, Pic: true, step: step + 1 })
+    }
+
+    picNext(imageUrl) {
+        let { step } = this.state;
+        this.setState({ imageUrl, Pic: false, Detail: true, step: step + 1 });
+
+    }
+
+    detailNext(selectedBeverages, selectedDuration) {
+        let { step } = this.state;
+        this.setState({ selectedBeverages, selectedDuration, Detail: false, step: step + 1 });
+
+    }
+
     render() {
-        const { userData, loader } = this.state;
+        const { userData, loader, Bio, Pic, Detail, step } = this.state;
         console.log(this.state)
         return (
             <div>
                 <Navbar />
-                {/* {loader &&
+                {loader &&
                     <div className="loader-div">
                         <Loader
                             type="ThreeDots"
                             color="#FD5D62"
                         />
                     </div>
-                } */}
+                }
                 {
-                    // userData &&
+                    userData &&
                     <div>
                         <div className="get-start-div">
                             <div>
                                 <div className="pf-heading-div">Welcome</div>
                                 <div className="pf-heading1-div">Omer Khan</div>
                                 <div className="pf-para-div">Please complete your profile before starting.</div>
-                                <div className="pf-para1-div">Step 1/3</div>
+                                <div className="pf-para1-div">Step {step}/3</div>
                                 <div>
-                                    {/* <div className="pf-content-div">
-                                        <Biodata />
-                                    </div> */}
-                                    <div className="pf-content1-div">
-                                        <Picture />
-                                    </div>
+                                    {Bio && <div className="pf-content-div">
+                                        <Biodata bioNext={this.bioNext} />
+                                    </div>}
+
+                                    {Pic && <div className="pf-content1-div">
+                                        <Picture picNext={this.picNext} />
+                                    </div>}
+                                    {Detail &&<div className="pf-content2-div">
+                                        <Details detailNext={this.detailNext} />
+                                    </div>}
                                 </div>
                             </div>
 
